6fffa6ddb9344e88ac66e96c172cda3049aed89a
Sara Thornsberry <thornsberrysara@gmail.com>
2022-01-29 00:21:36 -0500

    Completed Pacman rules

---
+++ pacman-rules/lib/rules.ex
@@ -0,0 +1,18 @@
+defmodule Rules do
+  def eat_ghost?(power_pellet_active, touching_ghost) do
+    power_pellet_active and touching_ghost   
+  end
+
+  def score?(touching_power_pellet, touching_dot) do
+    touching_power_pellet or touching_dot
+  end
+
+  def lose?(power_pellet_active, touching_ghost) do
+    not power_pellet_active and touching_ghost
+  end
+
+  def win?(has_eaten_all_dots, power_pellet_active, touching_ghost) do
+    lost = lose?(power_pellet_active, touching_ghost)
+    has_eaten_all_dots and not lost
+  end
+end
