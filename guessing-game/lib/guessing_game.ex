0c5ac68c23e68587e25c4899fe44494eb7cbee70
thornsberrysara <thornsberrysara@gmail.com>
2022-02-01 16:48:30 -0500

    Completed the guessing game

---
+++ guessing-game/lib/guessing_game.ex
@@ -0,0 +1,9 @@
+defmodule GuessingGame do
+  def compare(secret_number, guess \\ :no_guess)
+  def compare(_secret_number, :no_guess), do: "Make a guess"
+  def compare(secret_number, guess) when guess === secret_number, do: "Correct"
+  def compare(secret_number, guess) when guess + 1 === secret_number, do: "So close"
+  def compare(secret_number, guess) when guess - 1 === secret_number, do: "So close"
+  def compare(secret_number, guess) when guess > secret_number, do: "Too high"
+  def compare(secret_number, guess) when guess < secret_number, do: "Too low"
+end
